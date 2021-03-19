46c361c7331231b0afce59b701473a9a0944e561
Matthew Stevens <36384238+SchnitzelB@users.noreply.github.com>
2021-03-19 16:53:35 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main extends FibonacciSequence{
+
+  public static void main(String[] args) {
+    
+    FibonacciSequence fibonacciSequence = new FibonacciSequence();
+
+    fibonacciSequence.fibCount();
+    fibonacciSequence.fibSequence(count);
+
+  }
+  
+}
